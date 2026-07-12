6734199a44d13612717380f0341afd20629fb3b3
malachy okwute <mokwute@ivytech.edu>
2026-07-12 17:25:45 -0400

    initial commit - mo

---
+++ script.js
@@ -0,0 +1,12 @@
+// define event listener that is triggered when the DOM is fully loaded upon visiting the website.
+document.addEventListener('DOMContentLoaded', async function() {
+  const response = await fetch('http://localhost:3000/api/songs');
+  const songs = await response.json();
+
+  let html = '';
+  for (let song of songs) {
+    html += `<li>${song.title} - ${song.artist}</li>`;
+  }
+  
+  document.querySelector('#addedSongs').innerHTML = html;
+});
